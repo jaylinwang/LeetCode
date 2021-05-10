b04846dc96e0ac13510c9a0392589cea5c71f2ba
jaylinwang <jaylin0019@gmail.com>
2021-05-11 00:41:44 +0800

    two sum

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  printWidth: 100,
+};
